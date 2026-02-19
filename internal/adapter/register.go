bc431d0f57eaf9e850f74cb987c2e453ff5f29a1
dualface <dualface@gmail.com>
2026-02-19 23:00:13 +0800

    chore: unregister openai provider

--- internal/adapter/register.go
+++ internal/adapter/register.go
@@ -3,7 +3,6 @@ package adapter
 import (
 	"github.com/user/subscriptions-monitor/internal/adapter/kimi"
 	"github.com/user/subscriptions-monitor/internal/adapter/minimax"
-	"github.com/user/subscriptions-monitor/internal/adapter/openai"
 	"github.com/user/subscriptions-monitor/internal/adapter/zenmux"
 	"github.com/user/subscriptions-monitor/internal/provider"
 )
@@ -12,5 +11,4 @@ func RegisterAll(r *provider.Registry) {
 	r.Register(zenmux.New())
 	r.Register(kimi.New())
 	r.Register(minimax.New())
-	r.Register(openai.New())
 }
